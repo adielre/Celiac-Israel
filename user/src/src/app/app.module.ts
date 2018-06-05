d5576970cfd3c5f9e35de0c6ea57339ad29535b8
Yudit <peyehudit@gmail.com>
2018-06-05 11:16:11 +0300

    replace src folder

---
+++ user/src/src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Page2Page } from '../pages/page2/page2';
+import { FilterPage } from '../pages/filter/filter';
+import { FormsModule } from '@angular/forms';
+
+//import {HttpClientModule} from '@angular/common/http'
+import { HttpModule } from '@angular/http';
+import { FirebaseService } from './firebase-service/firebase.service';
+
+//validation
+//import { Validators } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    Page2Page,
+    FilterPage,
+    //FirebaseService,
+    
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    FormsModule,
+  //  HttpClientModule
+    HttpModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    Page2Page,
+    FilterPage
+  ],
+  providers: [
+    FirebaseService,
+    StatusBar,
+    SplashScreen,
+  //  Validators,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ user/src/src/pages/home/home.ts
@@ -0,0 +1,103 @@
+import { Component, Input,ViewChild } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+import { Page2Page } from '../page2/page2';
+import { FirebaseService } from '../../app/firebase-service/firebase.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  
+  @ViewChild('searchBox') searchString
+  @Input() myLocation: string
+
+    locationForm: FormGroup;
+    loadPage2: any =0;
+
+    constructor(public navCtrl: NavController, private alertCtrl: AlertController,private firebase: FirebaseService, public formBuilder: FormBuilder) {
+      this.locationForm = formBuilder.group({
+         myLocation: ['', Validators.compose([Validators.pattern('[א-ת ]*'), Validators.required])],
+      });
+  }
+
+  openPlaceList() {
+
+    if(!this.locationForm.valid)//incorect valid
+      return;
+   
+      this.loadPage2=1;
+
+    this.firebase.queryFirestore().fromCollection('resturant').field('Address').equals(this.searchString.value).runQuery()
+      .then(function (res) {
+         if (res.length === 0) {
+          alert('NO RESTURANT IN THIS LOCATION!');
+          this.loadPage2=0;
+        } else {
+          //alert(this.result)
+          this.navCtrl.push(Page2Page, { result: res });
+          this.loadPage2=0;
+        }
+      }.bind(this))
+     
+  }
+
+
+  onLoadPlaces() {
+    if (this.myLocation == undefined) {
+      this.presentAlert();
+      //console.log("hello");
+    }
+    else {
+
+      this.myLocation = undefined;
+      this.navCtrl.push(Page2Page);
+      //console.log("world");
+    }
+  }
+
+ /* tryMe() {
+    console.log('Clicked')
+    window['firebase'].firestore().collection("resturant").add(
+      {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+      }
+    )
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+  }
+
+  createNewUser(email: string, password: string) {
+    window['firebase'].auth().createUserWithEmailAndPassword(email, password)
+  }*/
+
+  getRes(myLocation: string) {
+
+  }
+
+  presentAlert() 
+  {
+    
+    let alert = this.alertCtrl.create({
+      title: 'שגיאה',
+      subTitle: 'אתה חייב להכניס מיקום',
+      cssClass: 'rtlAlert',
+      buttons: ['אשר']
+    });
+    alert.present();
+    
+  }
+
+
+
+
+
+}
