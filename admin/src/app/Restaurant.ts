4a14e43bddc9dd686242e17cc67cf1738d407f58
hallel1 <halleli100@gmail.com>
2018-06-20 08:54:13 +0300

    fix kefel code

--- admin/src/app/Restaurant.ts
+++ admin/src/app/Restaurant.ts
@@ -26,9 +26,9 @@ export class Restaurant {
     lastmodi: string= '';
 
     constructor(restObj) {
-        this.name = restObj['namePlace']
+        this.name = restObj['name']
         this.phone = restObj['phone']
-        this.address = restObj['Address']
+        this.address = restObj['address']
         this.id = restObj.id
         this.lastmodi=restObj['lastmodi']
         this.email = restObj['email']
@@ -37,11 +37,11 @@ export class Restaurant {
             this.website = restObj['links']['website']
             this.imageUrl = restObj['links']['logo']
         }
-        this.openingTime = restObj['opening']
-        this.description = restObj['Description']
+        this.openingTime = restObj['openingTime']
+        this.description = restObj['description']
         this.priceRange = restObj['priceRange']
         this.restauranttype = restObj['restauranttype']
-        this.moreInformation = restObj['moreInfo']
+        this.moreInformation = restObj['moreInformation']
         this.additionalPreferences = restObj['sensitivePreferences']['preferences']
         this.sensitivePreferences = restObj['sensitivePreferences']['preferences']
         this.facilities = restObj['facilities']
--- admin/src/app/add-edit-screen/add-edit-screen.component.ts
+++ admin/src/app/add-edit-screen/add-edit-screen.component.ts
@@ -17,18 +17,15 @@ export class AddEditScreenComponent implements OnInit {
   } 
 
   $key: string;
-  namePlace: string;
-  Address: string;
+  name: string;
+  address: string;
   phone: string='';
   lastmodi: string='';
   email: string='';
   restauranttype: string='';
-  Description: string='';
-  opening: string='';
+  description: string='';
+  openingTime: string='';
  
-  /*opening: {openHour: number, openMin: number, closeHour: number, closeMin: number}={
-    openHour: 0, openMin: 0, closeHour: 0, closeMin: 0
-  }*/
   sensitivePreferences: {gfMenu: boolean,sensitivity?: string,preferences?: string, accessibility: boolean, kosher: boolean}={
     gfMenu: false/*Gluten free menu*/,sensitivity: "",preferences: "", accessibility: false, kosher: false
   }
@@ -43,20 +40,20 @@ export class AddEditScreenComponent implements OnInit {
   
   city: string ;
   facilities:  string='';
-  moreInfo:  string='';
+  moreInformation:  string='';
   TypeOfBusiness :  string='';
   
  
 public save(){
     let obj = {
-      namePlace: this.namePlace.trim(),
-      Address:this.Address.trim(),
+      name: this.name.trim(),
+      address:this.address.trim(),
       city: this.city.trim(),
       phone: this.phone.trim(),
       lastmodi:this.lastmodi.trim(),
       email: this.email.trim(),
-      opening: this.opening.trim(),
-      Description:this.Description.trim(),
+      openingTime: this.openingTime.trim(),
+      description:this.description.trim(),
       restauranttype: this.restauranttype.trim(),
       TypeOfBusiness:this.TypeOfBusiness.trim(),
 
@@ -65,19 +62,17 @@ public save(){
       links:  this.linksUrl,
       priceRange: this.priceRange.trim(),      
       facilities: this.facilities.trim(),
-      moreInfo: this.moreInfo.trim()
+      moreInformation: this.moreInformation.trim()
 
-      /*location_in_map: this.location_in_map,
-    */
     }
 
     this.isLoading = true
-    this.resColl = this.afs.collection("resturant").doc(this.namePlace).set(obj).then(res => {
+    this.resColl = this.afs.collection("resturant").doc(this.name).set(obj).then(res => {
       this.isLoading = false
       this.router.navigate(['/main-screen']) //  when the firebase returned, we will go back home
     })
 
-    //this.resColl.add(obj)
+
   }
   ngOnInit() {
     this.resColl = this.afs.collection("resturant");
